f584dfc3be76d9c5e777d2b71b4d925b7eb9e202
arthaista1 <ska8ear@gmail.com>
2026-05-08 17:06:42 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', () => {
+
+    const nav = document.querySelector('nav');
+
+    if (window.scrollY > 50) {
+
+        nav.classList.add('bg-[#1e293b]', 'shadow-lg');
+        nav.classList.remove('glass-panel');
+
+    } else {
+
+        nav.classList.add('glass-panel');
+        nav.classList.remove('bg-[#1e293b]', 'shadow-lg');
+
+    }
+
+});
